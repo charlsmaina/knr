84f364732c2ee25e84652fdc4face51fdd4c0c75
Charles Maina <mainacharles1496@gmail.com>
2025-02-05 18:13:40 +0300

    innit

---
+++ chapter_1/tri.c
@@ -0,0 +1,33 @@
+/* Write a program to copy its imput to its output, replacing each string
+   of one or more blanks by a single blank. */
+#include <stdio.h>
+
+int main(void)
+{
+  int ch, prev;
+
+  prev = 1;
+  while ((ch=getchar()) != EOF)
+  if(prev == ch)
+  {
+    if(prev != ' ')
+    {
+        putchar(ch);
+        prev = ch;
+
+    }
+    else
+    {
+        prev = ch ;
+    }
+  }
+  else
+  {
+    putchar(ch);
+        prev = ch;
+  }
+
+    
+  
+  return 0;
+}
