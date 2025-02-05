2e1df89e35953739e0a23e675ef29c0ee003cea3
Charles Maina <mainacharles1496@gmail.com>
2025-02-05 11:06:28 +0300

    A more succint version

--- chapter_1/for-loop.c
+++ chapter_1/for-loop.c
@@ -2,8 +2,14 @@
 int main(void)
 {
     int fahr;
+   
+    void action()
+    {
+      fahr = fahr + 20 ;
+      printf("%3d\t\t%6.1f\n",fahr,(5.0/9.0) * (fahr - 32));
+    }
     printf("Fahrenheit\tCelcius\n");
-    for(fahr = 0; fahr <= 300 ; fahr = fahr + 20 )
-    printf("%3d\t\t%6.1f\n",fahr,(5.0/9.0) * (fahr - 32));
+    for(fahr = 0; fahr <= 300 ; action() );
+    
     
 }
