7d691428eee8b18c5fd76633db15dc7e02e18eff
Charles Maina <mainacharles1496@gmail.com>
2025-02-05 10:49:06 +0300

    a one line code to print the content entered by the user

---
+++ chapter_1/cha-io.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+   // implementation using a for loop
+
+   
+   for(int c ;( c = getchar()) != EOF ; putchar(c));
+  
+     
+    //implementation using a  while loop
+
+    /*
+     while(ch != EOF )
+    {
+         putchar(ch); // prints a single character from its ascii value
+         ch = getchar();
+    }
+    */
+
+   
+    
+}
