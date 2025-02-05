40b8ac4667106d3543645a74928920706a684e80
Charles Maina <mainacharles1496@gmail.com>
2025-02-05 12:28:39 +0300

    final version

--- chapter_1/trials.c
+++ chapter_1/trials.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Size of long: %zu bytes\n", sizeof(long));
-
-     int num = 10000000000;  // 10 billion
-    printf("Number: %d\n", num);
-    printf("Age: %d \n",365*24);
-    return 0;
+    int nc;
+   for (nc = 0 ;getchar() != EOF ;++nc);
+   printf("Tatal: %d\n",nc);
 
 }
