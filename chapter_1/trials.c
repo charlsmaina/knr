7f11bc5ab8cb92ecffbb625c472756c63c82248a
Charles Maina <mainacharles1496@gmail.com>
2025-02-05 15:09:21 +0300

    counting the number of characters

--- chapter_1/trials.c
+++ chapter_1/trials.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
+//A PROGRAM TO COUNT THE NUMBER OF CHARACTERS ENTERED BY A USER
 int main() {
-    int nc;
-   for (nc = 0 ;getchar() != EOF ;++nc);
-   printf("Tatal: %d\n",nc);
+    int c_count;
+   for (c_count = 0 ;getchar() != EOF ;(++ c_count));
+   printf("Total: %d\n",c_count); //each line is added an extra \n character
 
 }
