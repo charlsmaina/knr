84936f27ff151a806f85f1c411f509100c25e8d8
Charles Maina <mainacharles1496@gmail.com>
2025-02-05 12:17:03 +0300

    tests

---
+++ chapter_1/trials.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of long: %zu bytes\n", sizeof(long));
+
+     int num = 10000000000;  // 10 billion
+    printf("Number: %d\n", num);
+    printf("Age: %d \n",365*24);
+    return 0;
+
+}
