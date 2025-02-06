ec45aab7bb0550074585505633c0d412e7f76134
Charles Maina <mainacharles1496@gmail.com>
2025-02-06 10:40:38 +0300

    init

--- chapter_1/ex1-12.c
+++ chapter_1/ex1-12.c
@@ -7,12 +7,27 @@
 #include <stdio.h>
 int main(void)
 {
+int prev = 0; //To track previous character if it was a newline ,a tab or a space
  int input_char;
- while((input_char = getchar()) != EOF) 
+ while((input_char = getchar()) != EOF) //reads input up to when Ctrl D is pressed to signal EOF
  {
-   if (input_char == ' ' || input_char == '\t' )
-   putchar('\n');
+   if (input_char == ' ' || input_char == '\t' || input_char == '\n' )//unique conditions to consider
+   {
+    if(prev != ' ' && prev != '\t' && prev!= '\n' )//eliminates double spaces, double tabs and empty newlines
+   {
+    putchar('\n');
+    prev = input_char;
+   }
    else
-   putchar(input_char);
+   prev = input_char;
+   }
+   
+
+else
+{
+putchar(input_char);
+prev=input_char;
+}
+   
  }
 }
